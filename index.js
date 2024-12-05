da2b3e53da0d316319ed3f7450799cc368a6d638
Rahul Aminu <rahul.aminu16@smk.belajar.id>
2024-12-05 15:01:43 +0800

    update and delete

--- index.js
+++ index.js
@@ -44,11 +44,37 @@ app.post("/mahasiswa", (req, res) => {
 })
 
 app.put("/mahasiswa", (req, res) => {
-  response(200, "INI PUT ATAU UPDATE DATA", res)
+  const { nim, namaLengkap, kelas, alamat } = req.body
+  const sql = `UPDATE mahasiswa SET nama_lengkap = '${namaLengkap}', kelas = '${kelas}', alamat = '${alamat}' WHERE nim = '${nim}' `
+
+  db.query(sql, (err, fields) => {
+    if (err) response(500, "invalid", "error", res)
+    if (fields?.affectedRows) {
+      const data = {
+        isSuccess: fields.affectedRows,
+        message: fields.message
+      }
+      response(200, data, "Data Update Successfully", res)
+    }else {
+      response(404, "user not found", "error", res)
+    }
+  })
 })
 
 app.delete("/mahasiswa", (req, res) => {
-  response(200, "INI DELETE DATA", res)
+  const { nim } = req.body
+  const sql = `DELETE FROM mahasiswa WHERE nim = ${nim}`
+  db.query(sql, (err, fields) => {
+    if (err) response(500, "invalid", "error", res)
+    if (fields?.affectedRows){
+      const data = {
+        isDeleted: fields.affectedRows
+      } 
+      response(200, data,"Data Deleted Successfully", res)
+    }else {
+      response(404, "not found", "mohon maaf", res)
+    }
+  })
 })
 
 app.listen(port, () => {
